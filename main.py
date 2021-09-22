7756ea515374a127b8748ac0b6f9d60d86e7678c
PetriTurpeinen <plexi20@gmail.com>
2021-09-22 12:38:42 +0300

    User can ask now multiple questions
    Syntax for questions:
    - .quizon <number> For example: quizon 4
    - If you enter .quizon without parameters or wrong parameters, bot will prompt with an error message.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 #from discord.ext import commands
 import os
-import files
+#import files
 import quizmethods
 from keepalive import keep_alive
 import replitdatabase
@@ -9,24 +9,22 @@ import replitdatabase
 client = discord.Client()
 
 #Variables
+#List for questions & answers
+qlist = [[],[],[]]
 #Used to check if quiz is active, set to off by default.
-qlist = ["",""]
 quizState = ".quizoff"
+qAmount = []
+counter = 0
 #--------------
 #Instances
 #--------------
-#File handling:
-#--------------
-#Make instace of files class
-file = files.Files("questions.txt")
-#Read a file
-questions = file.readFile()
 databasevalues = replitdatabase.Database()
+
 #---------------
 #Quizmethods
 #---------------
 #Make instance of quizmethod class to get a random question
-quizmethod = quizmethods.QuizMethods(questions)
+quizmethod = quizmethods.QuizMethods()
 #---------------
 # Discord events
 #---------------
@@ -42,43 +40,48 @@ async def on_message(message):
   
   global quizState
   global qlist
-
-  if message.content == ".randomquestion":
-    qlist = quizmethod.get_question()
-
-    await message.channel.send(qlist[0])
-    await message.channel.send(qlist[1])
- 
+  global counter
+  global qAmount
+ #Check whether quiz is on/off
   if message.content == ".quizstate":
-    await message.channel.send(quizState) 
+    await message.channel.send(quizState)
+
+  if message.content == ".help":
+    await message.channel.send("You can start the quiz by typing .quizon and number of questions (20 is maxmimum). For example: .quizon 4 .") 
 
   if message.content == ".quizoff":
     quizState = ".quizoff"
     await message.channel.send("You have set quiz off.")    
+  try:
+    if message.content.startswith(".quizon") and quizState == ".quizoff":
+      qAmount = message.content.split(" ")
+      if qAmount[1].isnumeric():
+        quizState = ".quizon"    
+        await message.channel.send("You have set quiz on.")
+        qlist = quizmethod.askOpenTriviaQuestions(int(qAmount[1]))          
+        await message.channel.send(qlist[0][counter])               
+        await message.channel.send(qlist[2][counter])      
+      else:
+        await message.channel.send("Please enter a numeric value after the question. For example: .quizon 4")
+  except:
+    await message.channel.send("You need to enter the amount of questions. For example: .quizon 4")
 
-  #Start quiz and ask a random question (reads qustions from text file)
-  #if message.content == ".quizon" and quizState == ".quizoff":
-    #quizState = ".quizon"    
-    #await message.channel.send("You have set quiz on.")           
-    #await message.channel.send(quizmethod.askRandomQuestion()
-  if message.content == ".quizon" and quizState == ".quizoff":
-    quizState = ".quizon"    
-    await message.channel.send("You have set quiz on.")
-    qlist = quizmethod.get_question()           
-    await message.channel.send(qlist[0])
-    await message.channel.send(qlist[2])      
 
 
   #Congratulate user for right answer and keep asking questions
-  if message.content == qlist[1] and quizState == ".quizon":    
+  if message.content == qlist[1][counter] and quizState == ".quizon" and counter < int(qAmount[1]):    
     databasevalues.addUserDB(message.author.name)
     #Message sent to the user if he/she answers correctly  
-    correctmsg = "That was correct answer. You have now: " + str(databasevalues.getPoints(message.author.name)) + " points"    
-    await message.channel.send(correctmsg)    
-    qlist = quizmethod.get_question()
-    #await message.channel.send(quizmethod.askRandomQuestion())
-    await message.channel.send(qlist[0])
-    await message.channel.send(qlist[2])         
+    correctmsg = "That was correct answer " + message.author.name + "! You have now: " + str(databasevalues.getPoints(message.author.name)) + " points"    
+    await message.channel.send(correctmsg)
+    if counter < int(qAmount[1]) - 1:
+      counter += 1        
+      await message.channel.send(qlist[0][counter])      
+      await message.channel.send(qlist[2][counter])
+    else:
+      quizState = ".quizoff"
+      counter = 0
+      await message.channel.send("Quiz has ended.")           
 
 #---------------
 #Discord bot token
@@ -86,9 +89,19 @@ my_secret = os.environ['TOKEN']
 #Just a function to keep the bot alive by pinging it every 5 minutes
 keep_alive()
 client.run(os.getenv('TOKEN'))
-#TODO
-#Add correct msg to quiz methods
-#Make database class and move database code there
+
+#Legacy code
+#Start quiz and ask a random question (reads qustions from text file)
+  #if message.content == ".quizon" and quizState == ".quizoff":
+    #quizState = ".quizon"    
+    #await message.channel.send("You have set quiz on.")           
+    #await message.channel.send(quizmethod.askRandomQuestion()
+#File handling:
+#--------------
+#Make instace of files class
+#file = files.Files("questions.txt")
+#Read a file
+#questions = file.readFile()
 
 
 
--- quizmethods.py
+++ quizmethods.py
@@ -1,33 +1,28 @@
 import random
-import replitdatabase
+#import replitdatabase
 import json
 import requests
 import html
 #Class made for a quiz bot
 class QuizMethods():  
 
-  def __init__(self, questions):
-    self.questions = questions 
-
-  #Choose a random question
-  def randomQuestion(self):
-    question_answer = random.choice(self.questions)
-    split_question_answer = question_answer.split("|")    
-    return split_question_answer
-
-  #Choose a random question and return the question
-  def askRandomQuestion(self):
-
-    databasevalues = replitdatabase.Database()
-    randomquestion = self.randomQuestion()
-    question = randomquestion[0]
-    answer = randomquestion[1]
-    databasevalues.addValues(question,answer)
-    databasevalues.getQuestion()
-
-    return databasevalues.getQuestion()
+  def __init__(self):
+    pass
+    
   #https://opentdb.com/api_config.php
   #Get a random question from open trivia database
+  def askOpenTriviaQuestions(self,amount):
+    
+    qMatrix = [[""]*amount,[""]*amount,[""]*amount]
+
+    for i in range(0,amount):
+      #Add question, right answer and choices to matrix
+      qList = self.get_question()
+      qMatrix[0][i] = qList[0]
+      qMatrix[1][i] = qList[1]
+      qMatrix[2][i] = qList[2]
+    return qMatrix
+
   def get_question(self):
     response = requests.get("https://opentdb.com/api.php?amount=1&type=multiple")
     json_data = json.loads(response.text)
@@ -39,7 +34,7 @@ class QuizMethods():
     for i in range(0,len(wrong_answers)):
       if i == 0:
         choices.append(html.unescape(str(right_answer)))
-        choices.append(html.unescape(str(wrong_answers[i])
+        choices.append(html.unescape(str(wrong_answers[i])))
       else:
         choices.append(html.unescape(str(wrong_answers[i])))
 
@@ -51,10 +46,29 @@ class QuizMethods():
       else:
         str_choices += str(choices[j])
         
-    qlist = []
-    qlist.append(question)
-    qlist.append(right_answer)
-    qlist.append(str_choices)    
+    qList = []
+    qList.append(question)
+    qList.append(right_answer)
+    qList.append(str_choices)    
 
-    return(qlist)
+    return(qList)
+
+#Legacy code
+  #Choose a random question and return the question
+  #def askRandomQuestion(self):
+
+  #   databasevalues = replitdatabase.Database()
+  #   randomquestion = self.randomQuestion()
+  #   question = randomquestion[0]
+  #   answer = randomquestion[1]
+  #   databasevalues.addValues(question,answer)
+  #   databasevalues.getQuestion()
+
+#legacy code
+  #Choose a random question
+  #def randomQuestion(self):
+  #  question_answer = random.choice(self.questions)
+  #  split_question_answer = question_answer.split("|")    
+  #  return split_question_answer
+  #return databasevalues.getQuestion()
 
